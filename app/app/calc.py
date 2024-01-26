29df6aa8c04afa713641d78d6af46c3424c3f59b
Raymond-Rana <nerazmgr77@gmail.com>
2024-01-26 22:37:21 +0545

    Added GitHub Actions.

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator funtions
+"""
+
+def add(x, y):
+    """Add x and y return result."""
+    return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,19 +1,19 @@
-# """
-# Samples tests
-# """
-# from django.test import SimpleTestCase
+"""
+Samples tests
+"""
+from django.test import SimpleTestCase
 
-# from app import cal
+from app import cal
 
-# class CalTest(SimpleTestCase):
+class CalTest(SimpleTestCase):
     
-#     """Test the cal module."""
-#     def test_add_numbers(self):
-#         res = cal.add(5,6)
+    """Test the cal module."""
+    def test_add_numbers(self):
+        res = cal.add(5,6)
 
-#         self.assertEqual(res,11)
+        self.assertEqual(res,11)
 
-#     def test_subtract_numbers(self):
-#         res = cal.subtract(13,10)
+    def test_subtract_numbers(self):
+        res = cal.subtract(13,10)
 
-#         self.assertEqual(res,3)
+        self.assertEqual(res,3)
