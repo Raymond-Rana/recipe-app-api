9306b902349af48e27e5d687b600f995bf980ba5
Raymond-Rana <nerazmgr77@gmail.com>
2024-01-26 22:41:17 +0545

    Added GitHub Actions.

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,17 +3,17 @@ Samples tests
 """
 from django.test import SimpleTestCase
 
-from app import cal
+from app import calc
 
 class CalTest(SimpleTestCase):
     
     """Test the cal module."""
     def test_add_numbers(self):
-        res = cal.add(5,6)
+        res = calc.add(5,6)
 
         self.assertEqual(res,11)
 
     def test_subtract_numbers(self):
-        res = cal.subtract(13,10)
+        res = calc.subtract(13,10)
 
         self.assertEqual(res,3)
