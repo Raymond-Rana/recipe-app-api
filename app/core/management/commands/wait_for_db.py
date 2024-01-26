72f1c8c2c0a823a60cd40b3362e0fa9e4d9153d7
Raymond-Rana <nerazmgr77@gmail.com>
2024-01-26 23:19:59 +0545

    Added GitHub Action

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,8 +11,9 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
-    def handle(self,*args, **options):
-        """Entrypoint  for command."""        
+    
+    def handle(self,*args,**options):
+        """Entrypoint for command."""        
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
@@ -24,4 +25,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
-    
